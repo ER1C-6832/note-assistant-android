592dbac4aa0be03e4e87db8ebf76f3bb93e1ff32
ER1C-6832 <shangdomo2-c@my.cityu.edu.hk>
2026-07-10 18:07:09 +0800

    fix bugs

--- .phase5_03_graphicslayer_fix_backup_20260710-180344/app/src/main/java/com/er1cmo/noteassistant/assistantui/AssistantEntryOverlay.kt
+++ .phase5_03_graphicslayer_fix_backup_20260710-180344/app/src/main/java/com/er1cmo/noteassistant/assistantui/AssistantEntryOverlay.kt
@@ -47,7 +47,7 @@ import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.draw.graphicsLayer
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
